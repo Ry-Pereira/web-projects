bed87a6df6d1b5ee9cc226143ed0fc0684ec8fc2
Ryan <ryantonypereira@gmail.com>
2026-05-31 20:01:16 -0500

    Update script.js
    Add JavaScript functionality to generate and display random numbers

--- foundataional-projects/random-number-generator-website/script.js
+++ foundataional-projects/random-number-generator-website/script.js
@@ -7,3 +7,21 @@
 // Sources: 
 // Date: 
 // Last Modified: 
+
+
+
+
+
+
+
+
+
+
+
+document.getElementById("generateNumberBtn").onclick = function() {
+    // Generate a random number between 1 and 100
+    var randomNumber = Math.floor(Math.random() * 100) + 1;
+    document.getElementById("numberDisplay").textContent = "Number: " + randomNumber;
+}
+
+
