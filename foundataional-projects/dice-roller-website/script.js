0e8905410c021fc3df087d73588465678a2893b9
Ryan <ryantonypereira@gmail.com>
2026-06-08 16:29:51 -0500

    Fix roll range and display dice images
    Correct getDiceRollValue to produce values 1–6 (was generating 1–7). Add displayDiceImage to render the appropriate face image for each roll. Update roll button handler to clear previous results, iterate by selected dice quantity, roll each die, append images and values to diceArrayValues, and update the displayed list of rolled dice. Minor cleanup: remove unused diceRandomValue usage and initialize diceArrayValues on each roll.

--- foundataional-projects/dice-roller-website/script.js
+++ foundataional-projects/dice-roller-website/script.js
@@ -12,7 +12,8 @@
 
 
 let diceRandomValue;
-let diceArrayValues = [];
+var diceArrayValues = [];
+
 
 
 
@@ -25,13 +26,58 @@ function playDiceRollSound(){
 
 
 function getDiceRollValue(){
-    return Math.floor(Math.random() * 7) + 1;
+    return Math.floor(Math.random() * 6) + 1;
 }
 
 
+function displayDiceImage(diceValue){
+    if(diceValue === 1){
+        document.getElementById("dice_images_container").innerHTML += '<img src="images/dice-six-faces-one.png" width="100px">';
 
+    }
+    else if(diceValue === 2){
+       document.getElementById("dice_images_container").innerHTML += '<img src="images/dice-six-faces-two.png" width="100px">';
+
+    }
+    else if(diceValue === 3){
+        document.getElementById("dice_images_container").innerHTML += '<img src="images/dice-six-faces-three.png" width="100px">';
+
+    }
+    else if(diceValue === 4){
+        document.getElementById("dice_images_container").innerHTML += '<img src="images/dice-six-faces-four.png" width="100px">';
+
+    }
+    else if(diceValue === 5){
+        document.getElementById("dice_images_container").innerHTML += '<img src="images/dice-six-faces-five.png" width="100px">';
+
+    }
+    else if(diceValue === 6){
+        document.getElementById("dice_images_container").innerHTML += '<img src="images/dice-six-faces-six.png" width="100px">';
+
+    }
+        
+
+}
 
 document.getElementById("roll_dice_button").onclick = function(){
+    diceArrayValues = [];
+    diceImageContainer = document.getElementById("dice_images_container").innerHTML = ""
+    
+
     playDiceRollSound();
-    diceRandomValue = getDiceRollValue();
+    
+    let diceQuantity = document.getElementById("dice_quantity").value;
+    for(let i = 0; i < diceQuantity; i++){
+        let rolledDiceValue = getDiceRollValue();
+        displayDiceImage(rolledDiceValue);
+        diceArrayValues.push(rolledDiceValue);
+    }
+
+    document.getElementById("dice_value_display").textContent = "Dice: " + diceArrayValues.join(", ");
+
+    for(let i = 0; i < diceQuantity; i++){
+
+    }
+    
+    
 }
